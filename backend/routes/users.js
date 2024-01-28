991b82604e529171d24c188095229722bc4efa0b
Shahrez Aezad <shahrezaezad@storm.lan>
2024-01-28 21:48:56 +0500

    Basic backend framework added. (Incomplete)

---
+++ backend/routes/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+let User = require('../models/users.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/login').post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const role = req.body.role;
+    const email = req.body.email;
+    const newUser = new User({username, password, role, email});
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          HR Analytics and Reporting
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
