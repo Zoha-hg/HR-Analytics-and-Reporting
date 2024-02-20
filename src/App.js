b88c714d7f655cb1f33b8e39e50dc6778d865d91
Shahrez Aezad <shahrezaezad@Storm.local>
2024-02-20 15:53:41 +0500

    Consolidated complete login and signup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Home from './components/Home';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
-import LoginPage2 from './components/LoginPage1/LoginPage1'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage2 />} />
-          {/* <Route path='/login2' element={<LoginPage2 />} /> */}
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </div>
