6c628929f474c3131c72826d905341c4038f54c4
EeshaKamran17 <25100199@lums.edu.pk>
2024-04-08 13:37:27 +0500

    CSS changes, logo.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import './Home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="/login">
-                <button>Login</button>
-            </Link>
-            <Link to="/signup">
-                <button>SignUp</button>
-            </Link>
-            <Link to="/form">
-                <button>Create a Form</button>
-            </Link>
-            {/* <Link to="/fill">
-                <button>Fill a Form</button>
-            </Link> */}
-            <Link to="/display">
-                <button>Display Forms</button>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to HR Data Drive</h1>
+        <p>Your one-stop solution for HR analytics and reporting.</p>
+      </header>
+      <section className="home-content">
+        <h3>Harness the power of data to optimize your workforce, improve decision-making, and drive success.</h3>
+      </section>
+      <section className="button-container">
+        <Link to="/signup" className="sign-up-button">
+          Sign Up
+        </Link>
+        <Link to="/login" className="login-button">
+          Login
+        </Link>
+      </section>
+      <footer className="home-footer">
+        <p>&copy; 2024 Data Darbar</p>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </footer>
+    </div>
+  );
 };
+
 export default Home;
